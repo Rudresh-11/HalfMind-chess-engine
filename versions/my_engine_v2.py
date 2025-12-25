1d2214548926556586789b1c4783909db6422832
Rudresh-11 <work.rudreshsuryawanshi@gmail.com>
2025-12-25 19:41:17 +0530

    cleaning code

--- versions/my_engine_v2.py
+++ versions/my_engine_v2.py
@@ -276,7 +276,7 @@ def minimax(board:chess.Board, depth, alpha, beta, maximizing_player):
         return min_eval
 
 
-def get_best_move(board: chess.Board, depth):
+def get_best_move_v2(board: chess.Board, depth):
     best_move = None
     max_eval = -math.inf
     min_eval = math.inf
